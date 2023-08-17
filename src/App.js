b15d2773460c4b4db2537accc786578302dbf78e
박서영(Tessa) <lilianapsy@naver.com>
2023-08-18 01:16:51 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          안녕하세요! 테스트용 리액트 입니다!
         </p>
         <a
           className="App-link"
